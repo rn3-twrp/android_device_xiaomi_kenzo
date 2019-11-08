811db1290d0666e55dc183c0fc1eaf3c84d6c02e
LokManSiu <bjsiu0617@gmail.com>
2019-11-09 00:07:03 +0800

    BoardConfig.mk: fix mouse pointer bug

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,7 @@ TW_INCLUDE_NTFS_3G := true
 TW_IGNORE_MISC_WIPE_DATA := true
 TW_USE_TOOLBOX := true
 TW_NO_USB_STORAGE := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_SCREEN_BLANK_ON_BOOT := true
 
 # Asian region languages
